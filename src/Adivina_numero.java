3b4edc0430d98f265be39315e3ceb3a1231cc449
idevcm <ignarrios@gmail.com>
2022-08-07 15:17:46 +0100

    limpieza de codigo

--- src/Adivina_numero.java
+++ src/Adivina_numero.java
@@ -7,24 +7,26 @@ import java.util.Scanner;
 public class Adivina_numero {
     public static void main(String[] args) {
 
-        int aleatorio=(int)(Math.random()*100);
+        int aleatorio = (int)(Math.random() * 100);
 
-        Scanner entrada=new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         int numero=0;
-        int intentos=0;
+
+
+        //Do While
 
         do{
             intentos++;
             System.out.println("Introduce un numero, por favor");
             numero=entrada.nextInt();
 
-            if(numero>aleatorio){
+            if(numero > aleatorio){
                 System.out.println("Mas bajo");
-            }else if(numero<aleatorio){
+            }else if(numero < aleatorio){
                 System.out.println("Mas alto");
             }
 
-        }while (numero!=aleatorio);
+        }while (numero != aleatorio);
         System.out.println("Correcto. Lo has conseguido en "+ intentos + " intentos");
     }
 }
