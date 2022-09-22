5c4cd82a0e8d998aa4c2feb942ef65b43ebf09a0
idevcm <ignarrios@gmail.com>
2022-09-22 19:36:31 +0100

    cambios

---
+++ src/Comprueba_mail.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Comprueba_mail {
+    public static void main(String[] args) {
+        boolean arroba = false;
+
+        String mail = JOptionPane.showInputDialog("Introduce mail");
+
+        for (int i = 0; i < mail.length(); i++){
+
+            if(mail.charAt(i) == '@'){
+                arroba = true;
+            }
+        }
+        if(arroba == true) {
+            System.out.println("Es correcto");
+        }else {
+            System.out.println("No es correcto");
+        }
+    }
+}
--- src/Uso_Bucle_For.java
+++ src/Uso_Bucle_For.java
@@ -1,6 +1,11 @@
 public class Uso_Bucle_For {
     public static void main(String[] args) {
+        /**
+         * Imprimir 10 veces nuestro nombre una debajo de otra
+         */
+        for (int i = 0; i < 10; i++ ){
 
-        for (int i, )
+            System.out.println("Juan");
+        }
     }
 }
